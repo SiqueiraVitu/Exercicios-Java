762acd855be792caeaf3768de57218c6521676fd
SiqueiraVitu <vitorsiqueiravieira4@gmail.com>
2025-03-11 23:39:28 -0300

    Update Ex001.java

--- Ex001.java
+++ Ex001.java
@@ -1,7 +1,5 @@
 public class Ex001 {
     public static void main(String[] args) {
-        //Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
-
         System.out.println("Olá, Mundo!");
     }
 }
