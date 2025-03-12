0b6793d83b7b129dcf88824dd7be4ea48ac8260c
SiqueiraVitu <vitorsiqueiravieira4@gmail.com>
2025-03-11 23:39:09 -0300

    Update Ex002.java

--- Ex002.java
+++ Ex002.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Ex002 {
     public static void main(String[] args) {
-        //Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-
-        //vindas para ela:
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
         String nome = sc.nextLine();
