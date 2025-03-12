156d72a933cc3144ef5e502c923be3eecc2691bc
SiqueiraVitu <vitorsiqueiravieira4@gmail.com>
2025-03-11 23:41:24 -0300

    Update Ex009.java

--- Ex009.java
+++ Ex009.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class Ex009 {
     public static void main(String[] args) {
 
-        // 9.Faça um programa em Java que leia quanto dinheiro uma pessoa tem na carteira (em R$)
-        //e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45. (ou cotação
-        //atual)
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite o valor que está em sua carteira: ");
