20f4aed4060ae2032e047c2f2f4662afffcace0a
SiqueiraVitu <vitorsiqueiravieira4@gmail.com>
2025-03-11 23:40:40 -0300

    Update Ex007.java

--- Ex007.java
+++ Ex007.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class Ex007 {
     public static void main(String[] args) {
-        //7 Crie um programa em Java que leia um número real e mostre na tela o seu dobro e a sua
-        //terça parte.
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um numero: ");
         float num = sc.nextFloat();
